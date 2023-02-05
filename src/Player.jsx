1bab0c7071c663f8a0873290193514b9a4302e7d
priegelsdev <priegelsdev@gmail.com>
2023-02-05 15:23:08 +0100

    add some more styling

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,6 +1,6 @@
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Tooltip } from 'recharts'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircle as fasCircle, faInfo as fasInfo } from '@fortawesome/free-solid-svg-icons'
+import { faInfo as fasInfo } from '@fortawesome/free-solid-svg-icons'
 import { faPen as fasPen } from '@fortawesome/free-solid-svg-icons'
 
 export default function Player(props) {
@@ -21,26 +21,32 @@ export default function Player(props) {
       <h3 className="card-name">{props.name}</h3>
 
       <RadarChart cx={190} cy={190} outerRadius={170} width={380} height={380} data={data}>
-        <PolarGrid />
-        <PolarAngleAxis dataKey="stat" />
+        <PolarGrid 
+          stroke="black"
+        />
+        <PolarAngleAxis 
+          dataKey="stat"
+          tick={{fill: "black", fontWeight: 'bold'}}
+        />
+        <Radar 
+          name={props.name} 
+          dataKey="A" 
+          stroke="purple" 
+          fill="yellow" 
+          fillOpacity={0.6} 
+          dot={{ fill: 'white' }}  
+        />
         <PolarRadiusAxis 
+          stroke="gray"
           angle={90} 
           domain={[0, 100]} 
           tick={
             ({ x, y, payload }) => (
               <g transform={`translate(${x + 15},${y})`}>
-                <text x={0} y={0} dy={16} textAnchor="end" fill="black">{payload.value}</text>
+                <text x={0} y={0} dy={16} textAnchor="end" fill="black" fontWeight='bold'>{payload.value}</text>
               </g>
             )
           }
-        />
-        <Radar 
-          name={props.name} 
-          dataKey="A" 
-          stroke="#8884d8" 
-          fill="green" 
-          fillOpacity={0.6} 
-          dot={{ fill: 'white' }}  
         />
         <Tooltip />
       </RadarChart>
