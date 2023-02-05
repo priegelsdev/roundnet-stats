e787990c9635a691151c7866f1a6e00b3ad1cf31
priegelsdev <priegelsdev@gmail.com>
2023-02-05 13:27:44 +0100

    offset polar radius axis a bit to the right for better readability

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,4 +1,4 @@
-import { Radar, RadarChart, PolarGrid, Legend, PolarAngleAxis, PolarRadiusAxis, Tooltip } from 'recharts';
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Tooltip } from 'recharts';
 
 export default function Player(props) {
 
@@ -9,7 +9,7 @@ export default function Player(props) {
     { stat: 'Hitting', A: `${props.hitting}`*10},
     { stat: 'Short Defense', A: `${props.shortDefense}`*10},
     { stat: 'Long Defense', A: `${props.longDefense}`*10},
-  ];
+  ]
 
   return (
 
@@ -18,22 +18,30 @@ export default function Player(props) {
       <RadarChart cx={300} cy={250} outerRadius={150} width={500} height={500} data={data}>
         <PolarGrid />
         <PolarAngleAxis dataKey="stat" />
-        <PolarRadiusAxis />
-        <Radar name={props.name} dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
-        <Legend />
+        <PolarRadiusAxis 
+          angle={90} 
+          domain={[0, 100]} 
+          tick={
+            ({ x, y, payload }) => (
+              <g transform={`translate(${x + 15},${y})`}>
+                <text x={0} y={0} dy={16} textAnchor="end" fill="black">{payload.value}</text>
+              </g>
+            )
+          }
+        />
+        <Radar 
+          name={props.name} 
+          dataKey="A" 
+          stroke="#8884d8" 
+          fill="green" 
+          fillOpacity={0.6} 
+          dot={{ fill: 'white' }}  
+        />
         <Tooltip />
       </RadarChart>
 
       <div className="card-details">
         <h3 className="card-name">{props.name}</h3>
-
-        <div className="card-stats">
-          <span className="serving">Serving: {props.serving}</span>
-          <span className="serve-receiving">Serve Receiving: {props.serveReceiving}</span>
-          <span className="setting">Setting: {props.setting}</span>
-          <span className="hitting">Hitting: {props.hitting}</span>
-          <span className="defense">Defense: {(props.shortDefense + props.longDefense) / 2}</span>
-        </div>
 
         <div className="card-individuals">
           <p className="strengths">Strengths: {props.strengths}</p>
